14570e453ab57e142926a4ca52fadfe8fda32446
Aman Bhargava <amanbhargava2001@gmail.com>
2021-11-29 17:41:26 +0530

    Fixed indenting Markdown files

--- create_dataset.py
+++ create_dataset.py
@@ -1,79 +1,8 @@
-import os
-from slugify import slugify
+def create_dataset(current_path):
+    """
+    This function creates a new dataset in the datasets folder.
+    """
 
-# Get current path
-current_path = os.getcwd()
-
-# Switch to /datasets
-os.chdir("./datasets")
-
-
-# List out possible licenses
-licenses = [
-    "CC0",
-    "CC BY",
-    "CC BY-SA",
-    "CC BY-ND",
-    "CC BY-NC",
-    "CC BY-NC-SA",
-    "CC BY-NC-ND",
-]
-
-
-category_list = [
-    "categories",
-    "cities",
-    "states"
-]
-
-
-# Function to find names of folders in a directory
-def get_folders(path):
-    folders = []
-    for folder in os.listdir(path):
-        if os.path.isdir(os.path.join(path, folder)):
-            folders.append(folder)
-    return folders
-
-
-
-
-README_TEMPLATE = """
-# {dataset_name}
-#### {category} > {subcategory}
-
-{data_description}
-
-## Download
-
-Install the `desidata` package using CRAN and then call the `download_data()` function:
-```r
-library(desidata)
-download_data(name = "{dataset_id}")
-```
-
-## Data Dictionary 
-
-{markdown_table_from_dataset_columns}
-
-# Source
-This dataset is sourced from {dataset_source}
-
-# License
-{dataset_license}
-
-## Cite This Dataset
-{dataset_citation}
-
-
-`desidata for R`
-
-"""
-
-# Function to take in user input about the dataset
-
-
-def get_dataset_info():
 
     while True:
         dataset_name = input("Name: ")
@@ -124,25 +53,26 @@ def get_dataset_info():
         for i, category in enumerate(category_list):
             print(f"{i}: {category}")
         category_index = input("Category: ")
+        dataset_category = category_list[int(category_index)]
         if category_index == "":
             print("Please enter a valid category")
         else:
             break
 
-
     while True:
        # Show list of subcategories and ask user to choose based on index
         print("\nChoose a subcategory")
         # Go to the category folder and get list of subcategories
-        subcategories = get_folders(os.path.join(current_path, category_list[int(category_index)]))
+        subcategories = get_folders(os.path.join(
+            current_path, category_list[int(category_index)]))
         for i, subcategory in enumerate(subcategories):
             print(f"{i}: {subcategory}")
         subcategory_index = input("Subcategory: ")
+        dataset_about = subcategories[int(subcategory_index)]
         if subcategory_index == "":
             print("Please enter a valid subcategory")
         else:
             break
-        
 
     dataset_columns = []
     while True:
@@ -152,23 +82,27 @@ def get_dataset_info():
         dataset_columns.append([column_name, column_type, column_description])
         if input("Add another column? (y/n) ") == "n":
             break
-    
+
     # Create citation for dataset
-    dataset_citation = "{dataset_source_name} ({dataset_date}). {dataset_name}. {dataset_category}. Retrieved from {dataset_source} \n".format(
+    dataset_citation = "{dataset_source_name} ({dataset_date}). '{dataset_name}', {dataset_url}. Retrieved from {dataset_source} \n".format(
         dataset_source_name=dataset_source_name,
         dataset_date=dataset_date,
         dataset_name=dataset_name,
         dataset_category=dataset_category,
-        dataset_source=dataset_source
+        dataset_source=dataset_source,
+        dataset_url=base_url.format(
+            category=dataset_category,
+            subcategory=dataset_about,
+            url=slugify(dataset_name)
+        )
     )
-    
 
     # Create a markdown table from the dataset columns
     markdown_table_from_dataset_columns = "| Column Name | Column Type | Column Description |\n| ----------- | ----------- | --------------- |\n"
     for column in dataset_columns:
         markdown_table_from_dataset_columns += "| " + \
             column[0] + " | " + column[1] + " | " + column[2] + " |\n"
-    
+
     # Create ID for dataset
     dataset_id = "dd" + "_" + dataset_name + "_" + dataset_date
     dataset_id = slugify(dataset_id)
@@ -207,11 +141,81 @@ def get_dataset_info():
                                        dataset_citation=dataset_citation,
                                        dataset_source=dataset_source_name,
                                        dataset_license=dataset_license,
-                                       category = dataset_category,
-                                       subcategory = dataset_about))
-        
+                                       category=dataset_category,
+                                       subcategory=dataset_about))
+
         # Switch back to current_path
-        # os.chdir(current_path)
+        os.chdir(current_path)
+
+if __name__ == "__main__":
+    import os
+    from slugify import slugify
+
+    current_path = os.getcwd()
+
+    # Switch to /datasets
+    os.chdir("./datasets")
+    # List out possible licenses
+    licenses = [
+        "CC0",
+        "CC BY",
+        "CC BY-SA",
+        "CC BY-ND",
+        "CC BY-NC",
+        "CC BY-NC-SA",
+        "CC BY-NC-ND",
+    ]
 
 
-get_dataset_info()
+    category_list = [
+        "categories",
+        "cities",
+        "states"
+    ]
+
+    base_url = "https://github.com/thedivtagguy/desidatasets/tree/master/datasets/{category}/{subcategory}/{url}"
+    # Function to find names of folders in a directory
+    def get_folders(path):
+        folders = []
+        for folder in os.listdir(path):
+            if os.path.isdir(os.path.join(path, folder)):
+                folders.append(folder)
+        return folders
+
+
+    README_TEMPLATE = """
+    # {dataset_name}
+    #### {category} > {subcategory}
+
+    {data_description}
+
+    ## Download
+
+    Install the `desidata` package using CRAN and then call the `download_data()` function:
+    ```r
+    library(desidata)
+    download_data(name = "{dataset_id}")
+    ```
+
+    ## Data Dictionary 
+
+    {markdown_table_from_dataset_columns}
+
+    # Source
+    This dataset is sourced from {dataset_source}
+
+    # License
+    {dataset_license}
+
+    ## Cite This Dataset
+    {dataset_citation}
+
+
+    `desidata for R`
+
+    """
+
+    # Function to take in user input about the dataset
+    # Get current path
+
+    create_dataset(current_path)
---
+++ datasets/categories/agriculture/dd-aman-b-42043/README.md
@@ -0,0 +1,22 @@
+# Aman B
+#### categories > agriculture
+wtrefygrgitlhrtuikirtkeguvniuhgruygehcvrtghutrgvjtrgher
+## Download
+Install the `desidata` package using CRAN and then call the `download_data()` function:
+```r
+library(desidata)
+download_data(name = "dd-aman-b-42043")
+```
+## Data Dictionary 
+| Column Name | Column Type | Column Description |
+| ----------- | ----------- | --------------- |
+| sfhjgbdgdfsfd | wrtwer |  |
+
+# Source
+This dataset is sourced from dfgjhdfkjg
+# License
+CC BY
+## Cite This Dataset
+dfgjhdfkjg (42043). 'Aman B', https://github.com/thedivtagguy/desidatasets/tree/master/datasets/categories/agriculture/aman-b. Retrieved from fgdgjifd 
+
+`desidata for R`
